efc964a5ce7886fc5ac5dd92ce68366578d8932d
Joss Moffatt <josshmoffatt@gmail.com>
2022-11-16 21:53:36 +0000

    :test_tube: Adding kotlin benchmark

---
+++ benchmarks/kotlin/jbmc-regression/ArithmeticException1/Main.kt
@@ -0,0 +1,10 @@
+import org.sosy_lab.sv_benchmarks.Verifier
+
+fun main(args: Array<String>) {
+    try {
+        val i = Verifier.nondetInt()
+        val j = 10 / i
+    } catch (exc: ArithmeticException) {
+        throw AssertionError()
+    }
+}
